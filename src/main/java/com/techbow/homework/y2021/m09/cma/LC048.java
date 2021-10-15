e81b677dd1382ef31ee01e85724329d05ae25e61
cma16 <cma16@tamu.edu>
2021-10-14 23:59:07 -0500

    OCT 14

---
+++ src/main/java/com/techbow/homework/y2021/m09/cma/LC048.java
@@ -0,0 +1,22 @@
+package com.techbow.homework.y2021.m09.cma;
+
+public class LC048 {
+    class Solution {
+        public void rotate(int[][] matrix) {
+            //要采用拉的方式，不能采用推的方式。180度和270度都是要注意坐标的对应关系即可。
+            helper(matrix, 0, matrix.length);
+        }
+        private void helper(int[][] matrix, int offset, int size){
+            if(size <= 1) return;
+            for(int i = 0; i < size-1; i++){
+                int temp =  matrix[offset][offset+i];
+                matrix[offset][offset+i] = matrix[offset + size -1 - i][offset];
+                matrix[offset + size -1 - i][offset] = matrix[offset+size-1][offset+size-1-i];
+                matrix[offset+size-1][offset+size-1-i] = matrix[offset+i][offset+size-1];
+                matrix[offset+i][offset+size-1] = temp;
+            }
+            helper(matrix, offset+1, size-2);
+        }
+
+    }
+}
