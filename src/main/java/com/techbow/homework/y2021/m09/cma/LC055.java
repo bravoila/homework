a5401f908a4050e1ae1d8b0cfdb1ba841020e1d3
cma16 <cma16@tamu.edu>
2021-10-21 20:30:40 -0500

    OCT 21

--- src/main/java/com/techbow/homework/y2021/m09/cma/LC055.java
+++ src/main/java/com/techbow/homework/y2021/m09/cma/LC055.java
@@ -1,6 +1,43 @@
 package com.techbow.homework.y2021.m09.cma;
 
 public class LC055 {
+//recursion
+    class Solution {
+        public boolean canJump(int[] nums){
+            if(nums == null || nums.length < 2) return true;
+            return canJump(nums, 0);
+        }
+
+        private boolean canJump(int[] array, int step){
+            if(step >= array.length - 1) return true;
+            for(int i = 1; i <= array[step]; i++){
+                if(canJump(array, step + i)) return true;
+            }
+            return false;
+        }
+    }
+
+//    DP
+    class Solution {
+        public boolean canJump(int[] nums){
+            //cc
+            int len = nums.length;
+            boolean[] dp = new boolean[len];
+            dp[len-1] = true;
+
+            for(int i = len-2; i >= 0; i--){
+                for(int j = 1; j <= nums[i]; j++){
+                    if(i+j >= len-1 || dp[i+j]) {
+                        dp[i] = true;
+                        break;
+                    }
+                }
+            }
+            return dp[0];
+        }
+    }
+
+//Greedy
     class Solution {
         public boolean canJump(int[] nums){
             if(nums == null || nums.length == 0) return true;
@@ -13,4 +50,6 @@ public class LC055 {
             return false;
         }
     }
+
+
 }
