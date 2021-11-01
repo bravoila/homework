e5c3061c1871686a2f2918c40ba397412bf401d3
cma16 <cma16@tamu.edu>
2021-11-01 12:15:14 -0500

    NOV 01

---
+++ src/main/java/com/techbow/homework/y2021/m09/cma/LC117.java
@@ -0,0 +1,58 @@
+public class LC117 {
+    /*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+    class Solution {
+        public Node connect(Node root) {
+            if(root == null){
+                return root;
+            }
+
+            Queue<Node> myqueue = new LinkedList<>();
+            myqueue.add(root);
+
+            while(!myqueue.isEmpty()){
+                int mysize = myqueue.size();
+
+                while(mysize -- > 0){
+                    Node node = myqueue.poll();
+                    if(mysize == 0){
+                        node.next = null;
+                    } else{
+                        node.next = myqueue.peek();
+                    }
+
+                    if(node.left != null){
+                        myqueue.add(node.left);
+                    }
+
+                    if(node.right != null){
+                        myqueue.add(node.right);
+                    }
+
+                }
+            }
+            return root;
+        }
+    }
+}
