42094159dc5447b2d18267092f0c63c588933407
cma16 <cma16@tamu.edu>
2022-01-23 21:40:48 -0600

    Jan 23 2022

--- src/main/java/com/techbow/homework/y2021/m09/cma/LC674.java
+++ src/main/java/com/techbow/homework/y2021/m09/cma/LC674.java
@@ -1,6 +1,7 @@
 package com.techbow.homework.y2021.m09.cma;
 
 public class LC674 {
+//    slow fast指针
     class Solution {
         public int findLengthOfLCIS(int[] nums) {
             if(nums.length == 0 || nums == null) return 0;
@@ -23,5 +24,45 @@ public class LC674 {
         }
     }
 
+//    DP
 
+    class Solution {
+        public int findLengthOfLCIS(int[] nums) {
+            if(nums == null || nums.length == 0) return 0;
+//         dp is the max length, use reuse and reset method.
+            int[] dp = new int[nums.length];
+            dp[0] = 1;
+            int max = dp[0];
+
+            for(int i = 1; i < nums.length; i ++){
+                if(nums[i] > nums[i - 1]){
+                    dp[i] = dp[i - 1] + 1;
+                } else{
+                    dp[i] = 1;
+                }
+                max = Math.max(max, dp[i]);
+            }
+
+            return max;
+        }
+    }
+// reuse
+    class Solution {
+        public int findLengthOfLCIS(int[] nums) {
+            if(nums == null || nums.length == 0) return 0;
+//         dp is the max length, use reuse and reset method.
+            int cur = 1;
+            int max = cur;
+
+            for(int i = 1; i < nums.length; i ++){
+                if(nums[i] > nums[i - 1]){
+                    cur++ ;
+                } else{
+                    cur = 1;
+                }
+                max = Math.max(max, cur);
+            }
+            return max;
+        }
+    }
 }
