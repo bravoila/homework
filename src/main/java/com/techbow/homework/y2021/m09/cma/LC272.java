d6127bb4a76d3ec5758d3c4ce22c15d916caf131
cma16 <cma16@tamu.edu>
2022-03-14 11:54:27 -0500

    Mar 13 2022

--- src/main/java/com/techbow/homework/y2021/m09/cma/LC272.java
+++ src/main/java/com/techbow/homework/y2021/m09/cma/LC272.java
@@ -16,6 +16,7 @@ public class LC272 {
      *     }
      * }
      */
+    //拉直的写法
     class Solution {
         public void inorder(TreeNode root, List<Integer> nums) {
             if (root == null) return;
@@ -35,6 +36,64 @@ public class LC272 {
                 }
             });
             return nums.subList(0, k);
+        }
+    }
+// 用stack
+    class Solution {
+        public List<Integer> closestKValues(TreeNode root, double target, int k) {
+            Stack<TreeNode> lst = new Stack<TreeNode>();
+            Stack<TreeNode> rst = new Stack<TreeNode>();
+            List<Integer> res = new ArrayList<>();
+
+            TreeNode curr = root;
+            while(curr != null){
+                if(curr.val < target){
+                    //go right
+                    lst.push(curr);
+                    curr = curr.right;
+                } else{
+                    //go left
+                    rst.push(curr);
+                    curr = curr.left;
+                }
+            }
+            // k most close
+            while(k-- > 0){
+                if(!lst.isEmpty() && !rst.isEmpty()){
+                    if(Math.abs(lst.peek().val - target) < Math.abs(rst.peek().val - target)){
+                        res.add(leftMinus(lst));
+                    }else{
+                        res.add(rightPlus(rst));
+                    }
+                } else if(!lst.isEmpty()){
+                    res.add(leftMinus(lst));
+                } else if(!rst.isEmpty()){
+                    res.add(rightPlus(rst));
+                } else{
+                    break;
+                }
+            }
+            return res;
+        }
+
+        private int leftMinus(Stack<TreeNode> st){
+            TreeNode top = st.pop();
+            TreeNode curr = top.left;
+            while(curr != null){
+                st.push(curr);
+                curr = curr.right;
+            }
+            return top.val;
+        }
+
+        private int rightPlus(Stack<TreeNode> st){
+            TreeNode top = st.pop();
+            TreeNode curr = top.right;
+            while(curr != null){
+                st.push(curr);
+                curr = curr.left;
+            }
+            return top.val;
         }
     }
 }
