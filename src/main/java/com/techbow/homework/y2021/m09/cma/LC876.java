2a67d4d5b77448fa9f5deffe8c56af1a1335fb59
cma16 <cma16@tamu.edu>
2022-04-27 10:55:55 -0500

    Apr 27 2022

---
+++ src/main/java/com/techbow/homework/y2021/m09/cma/LC876.java
@@ -0,0 +1,21 @@
+package com.techbow.homework.y2021.m09.cma;
+
+public class LC876 {
+    class Solution {
+        public ListNode middleNode(ListNode head) {
+            if(head == null || head.next == null){
+                return head;
+            }
+
+            ListNode slow = head;
+            ListNode fast = head;
+
+            while(fast != null && fast.next != null){
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+
+            return slow;
+        }
+    }
+}
