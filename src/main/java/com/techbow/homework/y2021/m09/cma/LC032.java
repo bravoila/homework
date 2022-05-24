a2cfd927ac3ea5b8a6275324ac55237804edecb5
cma16 <cma16@tamu.edu>
2022-05-23 22:24:18 -0500

    May 23 2022

--- src/main/java/com/techbow/homework/y2021/m09/cma/LC032.java
+++ src/main/java/com/techbow/homework/y2021/m09/cma/LC032.java
@@ -26,4 +26,50 @@ public class LC032 {
             return res;
         }
     }
+
+    // two way
+    class Solution {
+        public int longestValidParentheses(String s) {
+            // cc
+            if(s == null || s.length() == 0){
+                return 0;
+            }
+
+            int left = 0;
+            int right = 0;
+            int res = 0;
+
+            for(int i = 0; i < s.length(); i++){
+                if(s.charAt(i) == '('){
+                    left++;
+                } else{
+                    right++;
+                }
+                if(left == right){
+                    res = Math.max(res, 2 * right);
+                } else if(right > left){
+                    left = 0;
+                    right = 0;
+                }
+            }
+
+            left = 0;
+            right = 0;
+
+            for(int i = s.length() - 1; i >= 0; i--){
+                if(s.charAt(i) == '('){
+                    left++;
+                } else{
+                    right++;
+                }
+                if(left == right){
+                    res = Math.max(res, 2 * left);
+                } else if(left > right){
+                    left = 0;
+                    right = 0;
+                }
+            }
+            return res;
+        }
+    }
 }
